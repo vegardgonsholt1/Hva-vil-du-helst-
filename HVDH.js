fa8a24e8178a66ea73077931ca440cd47edbee5a
Andreassme <andreas.smedsrud@haugnett.no>
2020-11-18 09:47:43 +0100

    js

---
+++ HVDH.js
@@ -0,0 +1,118 @@
+//@ts-check
+
+const choice = [
+    {
+        question:"Not eat Pizza ever again."
+    },
+    {
+        question:"Not drink Coffe ever again."
+    },
+    {
+        question:"Drive a a car 5 minutes with closed eyes"
+    },
+    {
+        question:"Cross a water, on your bike",
+    },
+    {
+        question:"Lose the ability to speak"
+    },
+    {
+        question:"Lose the ability to read."
+    },
+    {
+        question:"Be covered in fur"
+    },
+    {
+        question:"Be covered in scales"
+    },
+    {
+        question:"Be in jail for a year."
+    },
+    {
+        question:"Lose a year of your life."
+    },
+    {
+        question:"Always be 10 minutes late"
+    },
+    {
+        question:"Always be 20 minutes early"
+    },
+    {
+        question:"Know the history of every object you touch"
+    },
+    {
+        question:"Be able to talk to animals"
+    },
+    {
+        question:"Give up drinking anything except water"
+    },
+    {
+        question:"Give up eating anything that was cooked in a oven"
+    },
+    {
+        question:"Be able to see 10 minutes in your future"
+    },
+    {
+        question:"Be able to see 10 minutes of anyone but yourself "
+    },
+    {
+        question:"Have an easy job, working for someone else"
+    },
+    {
+        question:"Work for yourself, but work incredibly hard"
+    },
+    {
+        question:"Be the first to explore a planet"
+    },
+    {
+        question:"Be the inventor of a drug that cures a deadly disease"
+    },
+    {
+        question:"Go back to age 5, with everything you know"
+    },
+    {
+        question:"Know now everything your future self will know"
+    },
+    {
+        question:"See what is after every closed door"
+    },
+    {
+        question:"Be able to guess the combination of every safe on the first try"
+    },
+    {
+        question:"Be a average person in the present",
+    },
+    {
+        question:"Be the king of a large country, but 2500 years ago"
+    },
+    {
+        question:"Be able to dodge anything"
+    },
+    {
+        question:"Be able to ask 3 questions, and get accurate answers."
+    },
+    {
+        question:"Be forced to dance every time you heard music"
+    },
+    {
+        question:"Be forced to sing along to any song you heard"
+    },
+    {
+        question:"Move in a new city or town every month"
+    },
+    {
+        question:"Never be able to leave the city or town you were born in"
+    },
+    {
+        question:"Be a OP character in virtual reallity"
+    },
+    {
+        question:"Be in real life, but you can't interact with anyone or anything"
+    },
+    {
+        question:"Be able to control fire"
+    },
+    {
+        question:"Be able to control water"
+    }
+];
